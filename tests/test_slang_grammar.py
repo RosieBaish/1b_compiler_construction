a76b539a9ebdc880349f5cdfff89dd410c1971b0
Rosie Baish <rosie.baish@192-168.co.uk>
2026-01-03 11:25:20 +0000

    Add the grammar for SLang, plus some tests to make sure it works
    The tests aren't really testing the code or the grammar, more just
    checks that I've not messed anything too major up.

---
+++ tests/test_slang_grammar.py
@@ -0,0 +1,99 @@
+from grammar_reader import Grammar
+
+from cfg import Token, Terminal
+from lexer import Lexer
+
+
+def test_terminals():
+    g = Grammar(filename="slang.grammar")
+
+    # Everything from lexer.mll in the slang repo except comments
+    expected_terminal_names = [
+        "WHITESPACE",
+        "LPAREN",
+        "RPAREN",
+        "COMMA",
+        "COLON",
+        "SEMICOLON",
+        "ADD",
+        "SUB",
+        "MUL",
+        "DIV",
+        "NOT",
+        "EQUAL",
+        "ASSIGN",
+        "LT",
+        "ANDOP",
+        "OROP",
+        "BAR",
+        "ARROW",
+        "WHAT",
+        "BANG",
+        "UNIT",
+        "TRUE",
+        "FALSE",
+        "REF",
+        "INL",
+        "INR",
+        "FST",
+        "SND",
+        "CASE",
+        "OF",
+        "IF",
+        "THEN",
+        "ELSE",
+        "LET",
+        "FUN",
+        "IN",
+        "BEGIN",
+        "END",
+        "WHILE",
+        "DO",
+        "BOOL",
+        "INTTYPE",
+        "UNITTYPE",
+        "INT",
+        "IDENT",
+    ]
+
+    expected_terminals = [Terminal(name) for name in expected_terminal_names]
+
+    for terminal in expected_terminals:
+        assert terminal in g.terminals
+    for terminal in g.terminals:
+        assert terminal in expected_terminals
+
+
+def test_lex():
+    g = Grammar(filename="slang.grammar")
+
+    lexer = Lexer(g.terminal_triples)
+
+    # Example if.slang from the slang repo
+    if_slang = """
+
+
+if ? = 0 then 17 else 21
+    """
+
+    expected = [
+        Token("IF"),
+        Token("WHAT"),
+        Token("EQUAL"),
+        Token("INT", "0"),
+        Token("THEN"),
+        Token("INT", "17"),
+        Token("ELSE"),
+        Token("INT", "21"),
+    ]
+
+    actual = lexer.lex(if_slang)
+
+    print(expected)
+    print()
+    print(actual)
+    print()
+
+    assert len(expected) == len(actual)
+    for e, a in zip(expected, actual):
+        assert e.identical_to(a)
